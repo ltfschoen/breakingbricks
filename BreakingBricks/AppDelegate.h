bf608065846a0a13b553bfdc4be2da0728f0da9a
ltfschoen <ltfschoen@gmail.com>
2014-06-16 07:30:06 +1000

    Initial Commit

---
+++ BreakingBricks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BreakingBricks
+//
+//  Created by Luke on 16/06/2014.
+//  Copyright (c) 2014 Ceenos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BreakingBricks/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BreakingBricks
+//
+
+//  Copyright (c) 2014 Ceenos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
