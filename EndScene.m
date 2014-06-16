a99a300dedc05ee628cb358e02d87e43525bbbf9
ltfschoen <ltfschoen@gmail.com>
2014-06-16 19:50:20 +1000

    Added Retry Game Button on Tap with Animation. Imported First scene. Added game over sound.

--- EndScene.m
+++ EndScene.m
@@ -7,6 +7,7 @@
 //
 
 #import "EndScene.h"
+#import "MyScene.h"
 
 @implementation EndScene
 
@@ -14,6 +15,10 @@
     if (self = [super initWithSize:size]) {
         
         self.backgroundColor = [SKColor blackColor];
+        
+        // play game over sound
+        SKAction *playSoundEnd = [SKAction playSoundFileNamed:@"gameover.caf" waitForCompletion:NO];
+        [self runAction:playSoundEnd];
         
         SKLabelNode *label = [SKLabelNode labelNodeWithFontNamed:@"Futura Medium"];
         label.text = @"GAME OVER!";
@@ -35,8 +40,28 @@
         [self addChild:shadow];
         
         [self addChild:label];
+        
+        // second label
+        SKLabelNode *tryAgain = [SKLabelNode labelNodeWithFontNamed:@"Futura Medium"];
+        tryAgain.text = @"Tap to Play Again";
+        tryAgain.fontColor = [SKColor whiteColor];
+        tryAgain.fontSize = 24;
+        tryAgain.position = CGPointMake(size.width/2, size.height/2 - 40);
+        
+        SKAction *moveLabel = [SKAction moveToY:(size.height/2 - 40) duration:2.0];
+        [tryAgain runAction:moveLabel]; // tell the label to run the action
+        
+        [self addChild:tryAgain];
     }
     return self;
+}
+
+// method to respond to touches on this scene
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    // instantiate object of first scene and imported, of current size
+    MyScene *firstScene = [MyScene sceneWithSize:self.size];
+    [self.view presentScene:firstScene transition:[SKTransition doorwayWithDuration:2.0]];
+    
 }
 
 @end
