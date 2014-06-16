cc626952401d1cc92877c840b9f9ae2083158267
ltfschoen <ltfschoen@gmail.com>
2014-06-16 12:16:24 +1000

    Refactored addBall into separate method from the scene initialisation

--- BreakingBricks/MyScene.m
+++ BreakingBricks/MyScene.m
@@ -10,10 +10,38 @@
 
 @implementation MyScene
 
+- (void)addBall:(CGSize)size {
+    // create new sprite node from image
+    SKSpriteNode *ball = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
+    
+    // create a CGPoint for sprite position grabbing size parameter
+    CGPoint myPoint = CGPointMake(size.width/2, size.height/2);
+    ball.position = myPoint;
+    
+    // add physics body to ball object before add to scene
+    // convenience method adds to Physics Body Property of Node
+    ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball.frame.size.width/2];
+    
+    // modify friction Settings
+    ball.physicsBody.friction = 0;
+    ball.physicsBody.linearDamping = 0; // 0.1 by default
+    ball.physicsBody.restitution = 1.0f;
+    
+    // add sprite node to scene
+    NSLog(@"%@", ball);
+    [self addChild:ball];
+    
+    // create and define vector as separate variable
+    // apply vector impulse to node ball object
+    CGVector myVector = CGVectorMake(20, 20); // simple C Struct so no pointer for variable name required with two floats converted to a Vector value at 45 degrees of a magnitude
+    [ball.physicsBody applyImpulse:myVector];
+}
+
+// scene initialiser and setting properties
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
-    
+        
         self.backgroundColor = [SKColor whiteColor];
         
         // add physics body to scene to serve as an invisible boundary
@@ -24,21 +52,8 @@
         // modified to the moon of 1.6ms^2 by pull down on y-axis
         self.physicsWorld.gravity = CGVectorMake(0, 0);
         
-        
-        // create new sprite node from image
-        SKSpriteNode *ball = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
-        
-        // create a CGPoint for sprite position grabbing size parameter
-        CGPoint myPoint = CGPointMake(size.width/2, size.height/2);
-        ball.position = myPoint;
-        
-        // add physics body to ball object before add to scene
-        // convenience method adds to Physics Body Property of Node
-        ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball.frame.size.width/2];
-        
-        // add sprite node to scene
-        NSLog(@"%@", ball);
-        [self addChild:ball];
+        // call addBall method to create, configure, and add the ball object to the scene
+        [self addBall:size];
         
     }
     return self;
